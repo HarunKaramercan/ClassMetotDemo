5731d807baec8dc29d31c684eba0e04f39c86ff9
ccshark22245@outlook.com <ccshark22245@outlook.com>
2021-06-05 23:02:08 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void List(Customer[] Customers)
+        {
+            foreach (var list in Customers)
+            {
+                Console.WriteLine("\nCustomer Id :" + list.Id+"\nName :"+ list.Name+ "\nSurname :" + list.Surname+ "\nCity :" + list.City);
+                Console.WriteLine("Customer Listed");
+            }
+        }
+        public void Add(Customer csadd)
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Customer Added Id "+csadd.Id);
+        }
+        public void Delete(Customer csdelete)
+        {
+            Console.WriteLine("Customer Deleted Id "+csdelete.Id);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Customer Customer1 = new Customer();
+            Customer1.Id = "1";
+            Customer1.Name = "Sinan";
+            Customer1.Surname = "Özoğlu";
+            Customer1.City = "İstanbul";
+            Customer Customer2 = new Customer();
+            Customer2.Id = "2";
+            Customer2.Name = "Sema";
+            Customer2.Surname = "Kaçan";
+            Customer2.City = "Ankara";
+            Customer Customer3 = new Customer();
+            Customer3.Id = "3";
+            Customer3.Name = "Kemal";
+            Customer3.Surname = "Boyarslan";
+            Customer3.City = "Manisa";
+
+            Customer[] c1 = new Customer[] { Customer1 };
+            CustomerManager Cust = new CustomerManager();
+            Cust.Add(Customer1);
+            Cust.List(c1);
+            Cust.Delete(Customer1);
+
+
+            Customer[] c2 = new Customer[] { Customer2 };
+            CustomerManager Cust2 = new CustomerManager();
+            Cust2.Add(Customer2);
+            Cust2.List(c2);
+            Cust2.Delete(Customer2);
+
+            Customer[] c3 = new Customer[] { Customer3 };
+            CustomerManager Cust3 = new CustomerManager();
+            Cust2.Add(Customer3);
+            Cust2.List(c3);
+            Cust2.Delete(Customer3);
+
+
+
+        }
+    }
+}
